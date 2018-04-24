4bd9d1971291a4e44cd94db5e3a17bcbb7029757
TuanNguyen <TuanNguyen@HOANGTUAN-PC>
2018-04-24 13:53:11 +0700

    Add project files.

---
+++ Bai01-BroadCast/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Bai01_BroadCast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket sock = new Socket(AddressFamily.InterNetwork,
+            SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint iep1 = new IPEndPoint(IPAddress.Broadcast, 9050);
+            EndPoint ipe = (EndPoint)iep1;
+            // IPEndPoint iep2 = new IPEndPoint(IPAddress.Parse("192.168.1.255"), 9050);
+            string hostname = Dns.GetHostName();
+            byte[] data = Encoding.ASCII.GetBytes(hostname);
+            sock.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, 1);
+            while (sock.Poll(10000000, SelectMode.SelectRead))
+            {
+                sock.SendTo(data, iep1);
+                //sock.SendTo(data, iep2);
+                while (true)
+                {
+                    sock.ReceiveFrom(data, ref ipe);
+                    string host = Dns.GetHostName();
+                    Console.WriteLine("Dia chi IP dang hoat dong:{0}.", host);
+                }
+            }
+
+            sock.Close();
+        }
+    }
+}
